c6e9dacd48c11c627b462b6efa91c363b5b265a5
wenonezh <VADIM.P.11111@gmail.com>
2026-03-26 18:25:48 +0300

    Настроил маршруты и вьюхи для галереи

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+</head>
+<body>
+    <h1>Добро пожаловать в Laravel!</h1>
+    <p>Сайт работает!</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GalleryController;
 
-Route::get('/gallery', [GalleryController::class, 'index'])->name('gallery.index');
+Route::get('/', [GalleryController::class, 'index'])->name('gallery.index');
